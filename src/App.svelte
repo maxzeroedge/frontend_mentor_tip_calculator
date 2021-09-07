1ec72ef4de76fb8fda26bf13561a98c373bdb7ba
Palash <maxzeroedge@gmail.com>
2021-09-08 00:51:47 +0530

    Initial Setup Commit

---
+++ src/App.svelte
@@ -0,0 +1,86 @@
+<main class="main-container">
+    
+</main>
+
+<style type="text/sass">
+    $primary-color: hsl(245, 75%, 52%)
+
+    .main-container
+        width: 100vw
+        height: 100vh
+        display: grid
+        justify-content: center
+        align-content: center
+        background-image: url('images/pattern-background-desktop.svg')
+        background-repeat: no-repeat
+        background-size: 100% 50%
+        .content-container
+            width: 450px
+            height: 100vh
+            max-height: 700px
+            display: grid
+            grid-template-rows: 11fr 24fr //220:480
+            border-radius: 20px
+            overflow: hidden
+            background-color: white
+            justify-self: center
+            align-self: center
+            .title-image
+                background-size: cover
+            .summary-container
+                display: grid
+                justify-content: center
+                align-content: space-around
+                padding: 0 50px
+                text-align: center
+                h1
+                    padding: 0
+                    margin: 0
+                .summary-text
+                    color: hsl(224, 23%, 55%)
+                .btn
+                    cursor: pointer
+                    font-size: 90%
+                    font-weight: bold
+                .plan-setting
+                    display: flex
+                    justify-content: space-around
+                    align-content: center
+                    border-radius: 16px
+                    background-color: hsl(225, 100%, 98%)
+                    padding: 16px
+                    .music-icon
+                        width: 50px
+                        height: 50px
+                        background-size: cover
+                    .plan-detail
+                        display: grid
+                        align-content: center
+                        h4, p
+                            padding: 0
+                            margin: 0
+                        p
+                            color: hsl(224, 23%, 55%)
+                    .change-btn
+                        color: hsl(245, 75%, 52%)
+                        display: grid
+                        align-items: center
+                .action-container
+                    .submit-btn
+                        background-color: hsl(245, 75%, 52%)
+                        color: white
+                        padding: 16px
+                        border-radius: 16px
+                        box-shadow: 0px 16px 20px 2px hsla(245, 75%, 52%, 20%)
+                        margin-bottom: 25px
+                        
+
+    @media only screen and (max-width: 500px)
+        .main-container
+            .content-container
+                width: 90%
+                height: min(600px, 100vh)
+                max-height: min(600px, 100vh)
+                .summary-container
+                    padding: 0 20px
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const sveltePreprocess = require('svelte-preprocess')
+const {sass} = require('svelte-preprocess-sass')
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(path.dirname(__filename), 'dist'),
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Order Summary"
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'images', 
+                    to: 'images'
+                }
+            ]
+        })
+    ],
+    resolve: {
+        // see below for an explanation
+        alias: {
+            svelte: path.resolve('node_modules', 'svelte')
+        },
+        extensions: ['.mjs', '.js', '.svelte'],
+        mainFields: ['svelte', 'browser', 'module', 'main']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(svelte)$/,
+                use: {
+                    loader: 'svelte-loader',
+                    options: {
+                        preprocess: sveltePreprocess({
+                            /* TODO: options */
+                            style: sass()
+                        })
+                    }
+                }
+            },
+            {
+                // required to prevent errors from Svelte on Webpack 5+, omit on Webpack 4
+                test: /node_modules\/svelte\/.*\.mjs$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public', 'images'),
+        },
+        compress: true,
+        port: 9000,
+    },
+}
